3e0ddfeb8b4b1d7382063184dfdfc806ef40a9f6
Atti16 <84785429+Atti16@users.noreply.github.com>
2025-01-29 11:07:56 +0100

    Add files via upload

---
+++ GoBike-WebShop-main/Backend/users.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    pass VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (id, email, pass) VALUES
+(101, 'atti@atti', 'pass'),
+(102, '', 'pass'),
+(103, 'test@test', 'test');
+
+CREATE TABLE bikes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+// Használj lekérdezéseket az adatok megtekintésére:
+
+SELECT * FROM users;
+SELECT * FROM bikes;
+SELECT * FROM rentals;
